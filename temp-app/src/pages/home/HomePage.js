a330a4b0db35fb2130299eb69c9531a7c8c785ae
Han Nguyen <nguye340@sheridancollege.ca>
2025-06-12 14:49:30 -0400

    Deploy to GitHub Pages

---
+++ temp-app/src/pages/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to FableSmith Studio</h1>
+      <p>Fables aren't found. They are forged.</p>
+      {/* Add your home page content here */}
+    </div>
+  );
+};
+
+export default HomePage;
